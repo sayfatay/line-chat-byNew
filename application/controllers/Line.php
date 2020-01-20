fd0995a36bab924d7d9432aecdb61984c1598aa3
Sarawut (New) <sarawut@tarad.com>
2020-01-20 14:35:00 +0700

    edit routes

--- application/controllers/Line.php
+++ application/controllers/Line.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require 'vendor/autoload.php';
 
 
-class Main extends CI_Controller {
+class Line extends CI_Controller {
 
 
     public function __construct()
